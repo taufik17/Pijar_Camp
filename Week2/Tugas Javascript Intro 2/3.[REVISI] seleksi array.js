22bfe4330691d1e8662ee1996a8a26d221346149
taufik17 <taufikagungsantoso17@gmail.com>
2022-08-31 13:28:17 +0700

    [REVISI] nomor 3 nested if

--- Week2/Tugas Javascript Intro 2/3.[REVISI] seleksi array.js
+++ Week2/Tugas Javascript Intro 2/3.[REVISI] seleksi array.js
@@ -1,5 +1,5 @@
 const seleksiNilai = (nilaiAwal, nilaiAkhir, dataArray) => {
-    const validation = typeof (nilaiAwal, nilaiAkhir) === "number" && Array.isArray(dataArray) === true;
+    const validation = typeof nilaiAwal === "number" && typeof nilaiAkhir === "number" && Array.isArray(dataArray) === true;
 
     function isDataTrue(data) {
         if (data.length > 5) {
@@ -21,23 +21,18 @@ const seleksiNilai = (nilaiAwal, nilaiAkhir, dataArray) => {
             search(nilai);
         } else if (nilaiAwal > nilaiAkhir && isDataTrue(dataArray)) {
             console.log('Nilai akhir harus lebih besar dari nilai awal');
-        } else if (nilaiAwal < nilaiAkhir && dataArray.length <= 5) {
+        } else if (nilaiAwal < nilaiAkhir && !isDataTrue(dataArray)) {
             console.log('Jumlah angka dalam dataArray harus lebih dari 5');
         } else {
-            console.log(`nilaiAwal harus lebih kecil dari nilaiAkhir dan dataArray harus lebih dari 5`);
+            console.log(`nilaiAwal harus lebih kecil dari nilaiAkhir & dataArray harus lebih dari 5`);
         }
     } else {
         console.log('Parameter yang dikirim salah, lihat: seleksiNilai(number, number, [array])');
     }
 };
 
-// seleksiNilai(5, 20, [2, "25", 4, 14, 17, 30, 8]);
-// seleksiNilai(15, 3, [2, 25, 4, 14, 17, 30, 8]);
-// seleksiNilai(5, 17, [2, 25, 4]);
-// seleksiNilai([2, 25, 4, 1, 30, 18]);
-
-seleksiNilai("-5", 20, [2, 25, 4, "14", 17, 30, 8])
+seleksiNilai("10", 20, [2, 25, 4, "14", 17, 30, 8])
 seleksiNilai(15, 3, [2, 25, 4, 14, 17, 30, 8])
 seleksiNilai(5, 17, [2, 25, 4])
-seleksiNilai(50, 17, [2, 25, ])
+seleksiNilai(50, 17, [2, 25,])
 seleksiNilai([2, 25, 4, 1, 30, 18])
